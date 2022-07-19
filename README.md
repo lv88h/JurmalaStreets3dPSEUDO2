28c537b2773b5b87ac6b2229487f17f394937f35
lv88h <r.mazitis@gmail.com>
2022-07-19 09:13:42 +0300

    Added my project

---
+++ README.md
@@ -0,0 +1,1 @@
+# JurmalaStreets3d
---
+++ postlocation.php
@@ -0,0 +1,21 @@
+<?php
+include 'db.php';
+
+
+  $dsn = 'mysql:dbname=game;host=127.0.0.1';
+  $user = 'root';
+  $password = 'parole123!';
+  $dbh = new PDO($dsn, $user, $password);
+  
+  //$SEARCH = $_GET['locatedata'];
+  $SEARCH = $_POST['locatedata'];
+  
+  $data = json_decode(file_get_contents("php://input"));
+  
+  
+	    $stmt = $dbh->prepare("UPDATE users SET location = :location WHERE username = :username");
+    $stmt->bindValue(':location', $SEARCH);
+    $stmt->bindValue(':username', $user_data['username']);
+    $stmt->execute();
+
+?>
